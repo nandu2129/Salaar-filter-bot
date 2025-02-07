3ba4094a2b9e11f4d8fd0c49fef1d08176d21d1c
nandu2129 <vaddenandu63@gmail.com>
2025-02-07 09:44:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/MovieSearchGrou
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('https://t.me/NaChannel4', '').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('', "")
+DATABASE_URI = environ.get('', "mongodb+srv://vaddenandu70:<Nandu2580>@nandubots.omnod.mongodb.net/?retryWrites=true&w=majority&appName=NanduBots")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Nandu")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #---------------------------------------------------------------
